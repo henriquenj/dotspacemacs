8bebe6bbf3d5e9f50f881d2aafc4d8dcdc6a1ff5
Henrique Jung <henriquenj@gmail.com>
2017-07-17 19:38:47 -0300

    Uncommitted zones are highlighted on the left side

--- init.el
+++ init.el
@@ -54,7 +54,8 @@ values."
      ;; spell-checking
      ;; syntax-checking
      (version-control :variables version-control-diff-tool
-                      'diff-hl version-control-global-margin t)
+                      'diff-hl version-control-global-margin t
+                      version-control-diff-side 'left)
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
