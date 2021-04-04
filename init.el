eb6f93546739b8076ebbb907f12787a164315711
Henrique Jung <henriquenj@gmail.com>
2021-04-04 12:42:53 +0200

    Make doom-gruvbox the default theme

--- init.el
+++ init.el
@@ -141,7 +141,7 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(monokai gruvbox gotham)
+   dotspacemacs-themes '(doom-gruvbox chocolate monokai gotham)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Set the theme for the Spaceline. Supported themes are `spacemacs',
